f7cdfdbdbbbbac23e4bff531f306c4d4fbd86ea3
Filipe <filipecameracastro@gmail.com>
2021-03-06 11:33:20 -0300

    mudando css para tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import Head from "next/head";
 import Image from 'next/image';
-import styles from "../styles/Home.module.css";
 
 import Products from './api/products.json';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>EcoGames</title>
         <link rel="icon" href="/favicon.ico" />
@@ -16,33 +15,33 @@ export default function Home() {
           rel="stylesheet"
         ></link>
       </Head>
-      <main className={styles.main}>
-        <div className={styles.boxCart}>
-          <h5 className={styles.cartValue}>0</h5>
+      <main>
+        <div>
+          <h5>0</h5>
           <Image src='/cart-icon.svg' width='30px' height='25px'/>
         </div>
       </main>
       <div>
         {Products.map(item => (
-          <div className={styles.boxProduct}>
+          <div>
             <div>
               <Image src={`/${item.image}`} width='180px' height='180px'/>
             </div>
             <div>
               <div>
-                <h4 className={styles.productTitle}>{item.name}</h4>
+                <h4>{item.name}</h4>
               </div>
               <div>
-                <h3 className={styles.productPreco}>Preço: R$ {item.price}</h3>
-                <p className={styles.productScore}>Score: {item.score}</p>
+                <h3>Preço: R$ {item.price}</h3>
+                <p>Score: {item.score}</p>
               </div>
-              <button className={styles.buttonAddCart}>Adicionar ao carrinho</button>
+              <button>Adicionar ao carrinho</button>
             </div>
           </div>
         ))}
       </div>
-      <footer className={styles.footer}>
-        <p className={styles.footerTitle}>EcoGames</p>
+      <footer>
+        <p>EcoGames</p>
       </footer>
     </div>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      gray: colors.coolGray,
+    },
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
