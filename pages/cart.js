f0ee6e133e33be79b7110c132d3765d859d8aeaf
Filipe <filipecameracastro@gmail.com>
2021-03-06 18:02:28 -0300

    criando a funcao de remover produto

--- pages/cart.js
+++ pages/cart.js
@@ -3,20 +3,37 @@ import { useContext, useEffect, useState } from "react";
 import AppGamesContext from "../contexts";
 
 export default function Cart() {
-  const { product, frete, handleFrete } = useContext(AppGamesContext);
+  const {
+    product,
+    frete,
+    handleProduct,
+    handleFrete,
+    handleProdQuant,
+    prodQuant,
+  } = useContext(AppGamesContext);
   const [subTotal, setSubTotal] = useState(0);
   const [total, setTotal] = useState(0);
-  useEffect(() => {
-    let prodtotal = 0
-    product.map(item => prodtotal = prodtotal + item.price)
-    setSubTotal(prodtotal)
 
-    if(subTotal > 250) {
-      handleFrete(0)
+  function removeCart(index) {
+    const array = Array.from(product);
+    array.splice(index, 1)
+    handleProduct(array);
+    handleProdQuant(prodQuant - 1);
+    handleFrete(frete - 10);
+  }
+  useEffect(() => {
+    let prodtotal = 0;
+    product.map((item) => (prodtotal = prodtotal + item.price));
+    setSubTotal(prodtotal);
+
+    if (subTotal > 250) {
+      handleFrete(0);
+    } else {
+      handleFrete(product.length * 10)
     }
 
     setTotal(subTotal + frete);
-  }, [subTotal, frete])
+  }, [subTotal, frete, product]);
   return (
     <div className="md:content box-border w-full">
       <main className="bg-gray-800 h-4 p-6 flex flex-row justify-start items-center w-full text-white">
@@ -26,11 +43,20 @@ export default function Cart() {
         <h1 className="text-gray-900 font-sans font-bold text-2xl mt-12 ml-3">
           Cart
         </h1>
-        {product.map((item) => (
-          <div className="flex flex-row items-center justify-between m-3">
-            <button className='bg-none'>Remover</button>
-            <h4>{item.name}</h4>
-            <p className="font-sans font-normal text-gray-800">R$ {item.price}</p>
+        {product.map((item, index) => (
+          <div className="flex flex-col items-end m-3" key={index}>
+            <button
+              className="bg-none text-red-700 float-right mb-1 text-sm font-sans"
+              onClick={() => removeCart(index)}
+            >
+              Remover
+            </button>
+            <div className="flex flex-row items-center justify-between w-full">
+              <h4>{item.name}</h4>
+              <p className="font-sans font-normal text-gray-800">
+                R$ {item.price}
+              </p>
+            </div>
           </div>
         ))}
         <div className="flex flex-row items-center justify-between m-3">
