6e105fa9166560bf187ab18e91a3fe1911000d1f
Filipe <filipecameracastro@gmail.com>
2021-03-06 00:05:17 -0300

    estilizando a home e add produtos

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Image from 'next/image';
 import styles from "../styles/Home.module.css";
 
+import Products from './api/products.json';
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -13,12 +15,22 @@ export default function Home() {
           rel="stylesheet"
         ></link>
       </Head>
-      <main>
-        <div>
-          <h5>0</h5>
+      <main className={styles.main}>
+        <div className={styles.boxCart}>
+          <h5 className={styles.cartValue}>0</h5>
           <Image src='/cart-icon.svg' width='30px' height='25px'/>
         </div>
       </main>
+      <div>
+        {Products.map(item => (
+          <div>
+            <div>
+              <Image src={`/${item.image}`} width='150px' height='120px'/>
+            </div>
+            <div></div>
+          </div>
+        ))}
+      </div>
       <footer className={styles.footer}>
         <p className={styles.footerTitle}>Filipe Camera</p>
       </footer>
